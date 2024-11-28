933822e3abb91e3d78fb8187b885b58599587dbb
desjoerd <sjoerd.meer@outlook.com>
2024-11-28 13:53:09 +0100

    Add DictionaryExtensions

---
+++ src/OptionalValues/Collections/DictionaryExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace OptionalValues.Collections;
+
+public static class DictionaryExtensions
+{
+    public static OptionalValue<T> GetOptionalValue<TKey, T>(this IDictionary<TKey, T> dictionary, TKey key)
+        => dictionary.TryGetValue(key, out T? value) ? new OptionalValue<T>(value) : OptionalValue<T>.Unspecified;
+
+    public static void AddOptionalValue<TKey, T>(this IDictionary<TKey, T> dictionary, TKey key, OptionalValue<T> value)
+    {
+        if (value.IsSpecified)
+        {
+            dictionary.Add(key, value.SpecifiedValue);
+        }
+    }
+}
