e7bdaac8d87e2cc29251e98b00bad11a806874fc
desjoerd <sjoerd.meer@outlook.com>
2025-08-24 22:04:53 +0200

    ✅ Add tests around custom json converters

--- test/OptionalValues.Tests/OptionalValueJsonConverterAttributeTest.cs
+++ test/OptionalValues.Tests/OptionalValueJsonConverterAttributeTest.cs
@@ -5,10 +5,32 @@ namespace OptionalValues.Tests;
 
 public class OptionalValueJsonConverterAttributeTest
 {
+    public class UpperCaseStringJsonConverter : JsonConverter<string>
+    {
+        public override string? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return reader.GetString()?.ToUpperInvariant();
+        }
+
+        public override void Write(Utf8JsonWriter writer, string value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToUpperInvariant());
+        }
+    }
+
     public class ExampleModel
     {
+        /// <summary>
+        /// JsonStringEnumConverter which is a JsonConverterFactory
+        /// </summary>
         [OptionalValueJsonConverter(typeof(JsonStringEnumConverter<ExampleEnum>))]
         public OptionalValue<ExampleEnum> EnumValue { get; set; }
+
+        /// <summary>
+        /// Normal converter
+        /// </summary>
+        [OptionalValueJsonConverter(typeof(UpperCaseStringJsonConverter))]
+        public OptionalValue<string> UpperStringValue { get; set; }
     }
 
     public enum ExampleEnum
@@ -25,19 +47,28 @@ public class OptionalValueJsonConverterAttributeTest
     {
         var model = new ExampleModel
         {
-            EnumValue = new OptionalValue<ExampleEnum>(ExampleEnum.Foo)
+            EnumValue = new OptionalValue<ExampleEnum>(ExampleEnum.Foo),
+            UpperStringValue = new OptionalValue<string>("value"),
         };
 
         var json = JsonSerializer.Serialize(model, JsonSerializerOptions);
-        Assert.Equal("""{"EnumValue":"Foo"}""", json);
+        Assert.Equal("""{"EnumValue":"Foo","UpperStringValue":"VALUE"}""", json);
     }
 
     [Fact]
     public void ShouldDeserializeWithCustomConverterOnProperty()
     {
-        var json = """{"EnumValue":"Bar"}""";
+        var json = """{"EnumValue":"Bar","UpperStringValue":"value"}""";
         ExampleModel model = JsonSerializer.Deserialize<ExampleModel>(json, JsonSerializerOptions)!;
 
         Assert.Equal(ExampleEnum.Bar, model.EnumValue.Value);
+        Assert.Equal("VALUE", model.UpperStringValue.Value);
     }
+}
+
+[JsonSerializable(typeof(OptionalValueJsonConverterAttributeTest.ExampleModel))]
+[JsonSerializable(typeof(OptionalValueJsonConverterAttributeTest.ExampleEnum))]
+[JsonSerializable(typeof(string))]
+public partial class OptionalValueJsonConverterAttributeTestWithSourceGeneratorJsonSerializationContext : JsonSerializerContext
+{
 }
